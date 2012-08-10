f97c9d33d8862145732372fea83bd048f17c6d6e
Charles King <bringking@gmail.com>
2012-08-10 16:34:59 -0700

    Added basic page navigation from launch page to game page.

--- slHackathonGame/Pages/LaunchPage.xaml.cs
+++ slHackathonGame/Pages/LaunchPage.xaml.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.Phone.Controls;
+﻿using System;
 
 namespace slHackathonGame.Pages
 {
-    public partial class LaunchPage : PhoneApplicationPage
+    public partial class LaunchPage
     {
         // Constructor
         public LaunchPage()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Event handler to navigate to game page.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PlayGame(object sender, System.Windows.RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Pages/GamePage.xaml", UriKind.Relative));
         }
     }
 }
