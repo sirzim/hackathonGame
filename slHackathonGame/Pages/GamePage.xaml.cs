ad8e0060fb08ac7ae8195e991754c76a3661443f
Charles King <bringking@gmail.com>
2012-08-11 01:24:13 -0700

    Added test method for moving player to finger

--- slHackathonGame/Pages/GamePage.xaml.cs
+++ slHackathonGame/Pages/GamePage.xaml.cs
@@ -1,21 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Threading;
-using FarseerPhysics.Common;
 using FarseerPhysics.Dynamics.Joints;
 using FarseerPhysics.Factories;
 using Microsoft.Xna.Framework;
 using Spritehand.FarseerHelper;
+using Point = Microsoft.Xna.Framework.Point;
 
 namespace slHackathonGame.Pages
 {
     public partial class GamePage
     {
+        private readonly List<DistanceJoint> _joints;
+        private PhysicsSprite _player;
+
+
         public GamePage()
         {
             InitializeComponent();
             Loaded += GamePageLoaded;
+            _joints = new List<DistanceJoint>();
         }
 
        
@@ -27,6 +33,20 @@ namespace slHackathonGame.Pages
                 LayoutRoot.GetValue(PhysicsControllerMain.PhysicsControllerProperty) as PhysicsControllerMain;
             //hook up initilized event
             physicsController.Initialized += PhysicsControllerInitialized;
+          
+        }
+
+        void _player_ManipulationCompleted(object sender, ManipulationCompletedEventArgs e)
+        { 
+            
+        var physicsController =
+                LayoutRoot.GetValue(PhysicsControllerMain.PhysicsControllerProperty) as PhysicsControllerMain;
+            SetupReleaseTarget(sender,physicsController,e);
+        }
+
+        void _player_ManipulationStarted(object sender, ManipulationStartedEventArgs e)
+        {
+          
         }
 
         private void PhysicsControllerInitialized(object source)
@@ -34,6 +54,10 @@ namespace slHackathonGame.Pages
             //get reference to controller
             var physicsController = source as PhysicsControllerMain;
 
+
+            _player = physicsController.PhysicsObjects["player"];
+            _player.ManipulationStarted += _player_ManipulationStarted;
+         //   _player.ManipulationCompleted += _player_ManipulationCompleted;
 
             //get reference to branch
             if (physicsController != null)
@@ -48,6 +72,7 @@ namespace slHackathonGame.Pages
             //get location they clicked on branch
             PhysicsSprite branch = physicsController.PhysicsObjects["branch"];
             PhysicsSprite branch1 = physicsController.PhysicsObjects["branch1"];
+          
             branch1.ManipulationCompleted += (sender, e) => SetupDistanceJoint(sender, physicsController, e);
             branch.ManipulationCompleted += (sender, e) => SetupDistanceJoint(sender, physicsController, e);
         }
@@ -57,8 +82,18 @@ namespace slHackathonGame.Pages
             //MessageBox.Show("Hit branch");
         }
 
-        private void SetupDistanceJoint(object sender, PhysicsControllerMain physMain, ManipulationCompletedEventArgs e)
+        private void SetupReleaseTarget(object sender, PhysicsControllerMain physMain, ManipulationCompletedEventArgs e)
         {
+
+            //remove old joints
+            if (_joints.Count > 0)
+            {
+                foreach (var jnt in _joints)
+                {
+                    physMain.DeleteObject(jnt);
+                }
+            }
+
             const float breakpoint = 1000f;
 
             //translate coordinates relative to screen
@@ -67,6 +102,72 @@ namespace slHackathonGame.Pages
 
             //get reference to player
             PhysicsSprite player = physMain.PhysicsObjects["player"];
+
+
+            var letgopoint = new System.Windows.Point((int)e.ManipulationOrigin.X + (int)e.TotalManipulation.Translation.X,
+                                       (int)e.ManipulationOrigin.Y + (int)e.TotalManipulation.Translation.Y);
+
+            var testBody = BodyFactory.CreateCircle(physMain.Simulator, 25, 1, physMain.ScreenToWorld(letgopoint));
+
+            //create distance joint between the two
+            DistanceJoint joint = JointFactory.CreateDistanceJoint(physMain.Simulator, player.BodyObject,
+                                                                   testBody, Vector2.Zero, Vector2.Zero);
+
+            joint.Frequency = 4.0f;
+            joint.DampingRatio = .5f;
+            joint.Breakpoint = breakpoint;
+            joint.CollideConnected = true;
+            joint.Broke += joint_Broke;
+            _joints.Add(joint);
+
+            //create tounge
+
+
+            //timer
+            var timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(33) };
+
+            timer.Tick += delegate
+            {
+                //joint broke
+                if (!joint.Enabled)
+                {
+                    timer.Stop();
+                    physMain.DeleteObject(joint);
+                    return;
+                }
+
+                //reduce distance
+                if (joint.Length <= 0f)
+                {
+
+                    timer.Stop();
+                }
+                joint.Length -= .1f;
+            };
+            timer.Start();
+        }
+
+        private void SetupDistanceJoint(object sender, PhysicsControllerMain physMain, ManipulationCompletedEventArgs e)
+        {
+
+            //remove old joints
+            if(_joints.Count > 0)
+            {
+                foreach (var jnt in _joints)
+                {
+                    physMain.DeleteObject(jnt);
+                }
+            }
+
+            const float breakpoint = 1000f;
+
+            //translate coordinates relative to screen
+            var touchedObject = sender as PhysicsSprite;
+            //var list = BoundaryHelper.GetPointsForElement(touchedObject.uiElement, e.ManipulationContainer, false);
+
+            //get reference to player
+            PhysicsSprite player = physMain.PhysicsObjects["player"];
+
 
 
             //create distance joint between the two
@@ -78,6 +179,7 @@ namespace slHackathonGame.Pages
             joint.Breakpoint = breakpoint;
             joint.CollideConnected = true;
             joint.Broke += joint_Broke;
+            _joints.Add(joint);
 
             //create tounge
             
@@ -98,7 +200,7 @@ namespace slHackathonGame.Pages
                                   //reduce distance
                                   if (joint.Length <= 0f)
                                   {
-                                      physMain.DeleteObject(joint);
+                                     
                                       timer.Stop();
                                   }
                                   joint.Length -= .1f;
@@ -117,6 +219,20 @@ namespace slHackathonGame.Pages
             // MessageBox.Show(list[0].ToString());
 
             // MessageBox.Show(e.ManipulationOrigin.ToString());
+        }
+
+        private void LayoutRoot_ManipulationCompleted(object sender, ManipulationCompletedEventArgs e)
+        {
+           
+            var physicsController =
+        LayoutRoot.GetValue(PhysicsControllerMain.PhysicsControllerProperty) as PhysicsControllerMain;
+
+            MessageBox.Show(
+                String.Format(
+                    "Silverlight Touchpoint: {0}\n Farseer Vector:{1}\n Farseer ScreenTopLeft: {2}\nFarseer ScreenBottomRight:{3}\nFarseer WorldTopLeft{4}\nFarseer WorldBottomRight{5}",e.ManipulationOrigin.ToString(),physicsController.ScreenToWorld(e.ManipulationOrigin).ToString(),BoundaryHelperBox2d.ScreenTopLeft,BoundaryHelperBox2d.ScreenBottomRight,BoundaryHelperBox2d.WorldTopLeft,BoundaryHelperBox2d.WorldBottomRight));
+
+
+            e.Handled = true;
         }
     }
 }
