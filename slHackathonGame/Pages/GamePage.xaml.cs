a2d97f819b8d0641f445e22885fe833c10e2c641
Charles King <bringking@gmail.com>
2012-08-10 17:26:07 -0700

    Added test branch, created bounding box to test Farseer body collision detection

--- slHackathonGame/Pages/GamePage.xaml.cs
+++ slHackathonGame/Pages/GamePage.xaml.cs
@@ -1,23 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using Microsoft.Phone.Controls;
+﻿using System.Windows;
+using Spritehand.FarseerHelper;
 
 namespace slHackathonGame.Pages
 {
-    public partial class GamePage : PhoneApplicationPage
+public partial class GamePage
     {
+        
         public GamePage()
         {
             InitializeComponent();
+            Loaded += GamePageLoaded;
+            
+        }
+
+        void GamePageLoaded(object sender, RoutedEventArgs e)
+        {
+            //get reference to physics controller
+            var physicsController = LayoutRoot.GetValue(PhysicsControllerMain.PhysicsControllerProperty) as PhysicsControllerMain;
+            //hook up initilized event
+            physicsController.Initialized += PhysicsControllerInitialized;
+           
+        }
+
+        void PhysicsControllerInitialized(object source)
+        {
+            //get reference to controller
+            var physicsController = source as PhysicsControllerMain;
+
+            //get reference to branch
+            if (physicsController != null)
+            {
+                var bbox = physicsController.PhysicsObjects["bbox"];
+
+                //hook up collision event
+                bbox.Collision += BranchCollision;
+            }
+        }
+
+        void BranchCollision(PhysicsSprite source, string collidedWith)
+        {
+            //MessageBox.Show("Hit branch");
         }
     }
 }
