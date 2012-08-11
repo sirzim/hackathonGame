cbd9b5f29d6e821698dd113020c9631f9e5ee56c
Charles King <bringking@gmail.com>
2012-08-11 12:24:25 -0700

    Merged Random branch code with master

--- slHackathonGame/Pages/GamePage.xaml.cs
+++ slHackathonGame/Pages/GamePage.xaml.cs
@@ -1,98 +1,171 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Interactivity;
 using System.Windows.Media;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using FarseerPhysics.Dynamics;
 using FarseerPhysics.Dynamics.Joints;
 using FarseerPhysics.Factories;
 using Microsoft.Xna.Framework;
 using Spritehand.FarseerHelper;
 using Spritehand.PhysicsBehaviors;
-using Point = Microsoft.Xna.Framework.Point;
+using slHackathonGame.UserControls;
+using Color = System.Windows.Media.Color;
+using Point = System.Windows.Point;
 
 namespace slHackathonGame.Pages
 {
     public partial class GamePage
     {
         private readonly List<DistanceJoint> _joints;
-        private PhysicsSprite _player;
-        private System.Windows.Point _startDrag;
+        private bool _touchingPlayer;
         private Ellipse _finger;
+        private PhysicsSprite _player;
+        private Vector2 _startDrag;
+        private Line _dragLine;
+
+        private static Random random = new Random();
+        private enum Side
+        {
+            Left = 0,
+            Right = 1,
+            size = 2
+        }
+        private PhysicsControllerMain _physicsController;
 
         public GamePage()
         {
             InitializeComponent();
             Loaded += GamePageLoaded;
             _joints = new List<DistanceJoint>();
+
+
+        
         }
 
-       
 
         private void GamePageLoaded(object sender, RoutedEventArgs e)
         {
             //get reference to physics controller
-            var physicsController =
+            _physicsController =
                 LayoutRoot.GetValue(PhysicsControllerMain.PhysicsControllerProperty) as PhysicsControllerMain;
+
+
             //hook up initilized event
-            physicsController.Initialized += PhysicsControllerInitialized;
+            _physicsController.Initialized += PhysicsControllerInitialized;
 
 
-           
+            //setup finger
+            _finger = new Ellipse
+                          {
+                              Name = "finger",
+                              Height = 50,
+                              Width = 50,
+                              StrokeThickness = 3,
+                              Stroke = new SolidColorBrush(Colors.Red)
+                          };
+
+            // Spawn initial branches
+            for (int i = 0; i < (random.Next(2) + 4); i++)
+            {
+                spawnBranch((Side)random.Next(2), random.Next(500));
+            }
+
+
         }
 
-    
+        private void spawnBranch()
+        {
+            spawnBranch((Side)random.Next((int)Side.size), 0);
+        }
 
-       
+        private void spawnBranch(Side side)
+        {
+            spawnBranch(side, 0);
+        }
+
+        private void spawnBranch(int y)
+        {
+            spawnBranch((Side)random.Next((int)Side.size), y);
+        }
+
+        private void spawnBranch(Side side, int y)
+        {
+            spawnBranch(
+                side,
+                y,
+                80 + random.Next(200),
+                30 + random.Next(10)
+            );
+        }
+
+        private void spawnBranch(Side side, int y, int width, int height)
+        {
+            Branch branch = new Branch()
+            {
+                Width = width,
+                Height = height,
+                Name = (Guid.NewGuid()).ToString()
+            };
+
+            branch.Location = new System.Windows.Point(
+                side == Side.Left ?
+                    (branch.Width / 2) :
+                    this.LayoutRoot.ActualWidth - (branch.Width / 2),
+                y
+            );
+
+            this.LayoutRoot.Children.Add(branch);
+            //LayoutRoot.Children
+        }
 
         private void PhysicsControllerInitialized(object source)
         {
-            //get reference to controller
-            var physicsController = source as PhysicsControllerMain;
+           
 
 
-            _player = physicsController.PhysicsObjects["player"];
-            _player.Collision += _player_Collision;
+            _player = _physicsController.PhysicsObjects["player"];
+            _physicsController.TimerLoop += PhysicsControllerTimerLoop;
+
+
+
+            //var branch = _physicsController.PhysicsObjects["branch"];
+            //branch.DoubleTap += BranchDoubleTap;
+
+
+
+        }
+
+        void BranchDoubleTap(object sender, GestureEventArgs e)
+        {
+            //var obj = sender as PhysicsSprite;
+            SetupDistanceJoint(sender);
+        }
+
+   
+
      
 
-            //get reference to branch
-            if (physicsController != null)
-            {
-                //get reference to bounding box
-                PhysicsSprite bbox = physicsController.PhysicsObjects["bbox"];
-
-                //hook up collision event
-                bbox.Collision += BranchCollision;
-            }
-
-            //get location they clicked on branch
-            PhysicsSprite branch = physicsController.PhysicsObjects["branch"];
-            PhysicsSprite branch1 = physicsController.PhysicsObjects["branch1"];
-          
-            branch1.ManipulationCompleted += (sender, e) => SetupDistanceJoint(sender, physicsController, e);
-            branch.ManipulationCompleted += (sender, e) => SetupDistanceJoint(sender, physicsController, e);
-        }
-
-        void _player_Collision(PhysicsSprite source, string collidedWith)
+        void PhysicsControllerTimerLoop(object source)
         {
-            MessageBox.Show("Show body touched the player");
+
+          //  var finger = _physicsController.PhysicsObjects["branch"];
+         
         }
 
-        private void BranchCollision(PhysicsSprite source, string collidedWith)
-        {
-            //MessageBox.Show("Hit branch");
-        }
 
+   
         private void SetupReleaseTarget(object sender, PhysicsControllerMain physMain, ManipulationCompletedEventArgs e)
         {
-
             //remove old joints
             if (_joints.Count > 0)
             {
-                foreach (var jnt in _joints)
+                foreach (DistanceJoint jnt in _joints)
                 {
                     physMain.DeleteObject(jnt);
                 }
@@ -108,10 +181,10 @@ namespace slHackathonGame.Pages
             PhysicsSprite player = physMain.PhysicsObjects["player"];
 
 
-            var letgopoint = new System.Windows.Point((int)e.ManipulationOrigin.X + (int)e.TotalManipulation.Translation.X,
-                                       (int)e.ManipulationOrigin.Y + (int)e.TotalManipulation.Translation.Y);
+            var letgopoint = new Point((int) e.ManipulationOrigin.X + (int) e.TotalManipulation.Translation.X,
+                                       (int) e.ManipulationOrigin.Y + (int) e.TotalManipulation.Translation.Y);
 
-            var testBody = BodyFactory.CreateCircle(physMain.Simulator, 25, 1, physMain.ScreenToWorld(letgopoint));
+            Body testBody = BodyFactory.CreateCircle(physMain.Simulator, 25, 1, physMain.ScreenToWorld(letgopoint));
 
             //create distance joint between the two
             DistanceJoint joint = JointFactory.CreateDistanceJoint(physMain.Simulator, player.BodyObject,
@@ -126,66 +199,6 @@ namespace slHackathonGame.Pages
 
             //create tounge
 
-
-            //timer
-            var timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(33) };
-
-            timer.Tick += delegate
-            {
-                //joint broke
-                if (!joint.Enabled)
-                {
-                    timer.Stop();
-                    physMain.DeleteObject(joint);
-                    return;
-                }
-
-                //reduce distance
-                if (joint.Length <= 0f)
-                {
-
-                    timer.Stop();
-                }
-                joint.Length -= .1f;
-            };
-            timer.Start();
-        }
-        private void SetupDistanceJoint(object sender, PhysicsControllerMain physMain, ManipulationCompletedEventArgs e)
-        {
-
-            //remove old joints
-            if(_joints.Count > 0)
-            {
-                foreach (var jnt in _joints)
-                {
-                    physMain.DeleteObject(jnt);
-                }
-            }
-
-            const float breakpoint = 1000f;
-
-            //translate coordinates relative to screen
-            var touchedObject = sender as PhysicsSprite;
-            //var list = BoundaryHelper.GetPointsForElement(touchedObject.uiElement, e.ManipulationContainer, false);
-
-            //get reference to player
-            PhysicsSprite player = physMain.PhysicsObjects["player"];
-
-
-
-            //create distance joint between the two
-            DistanceJoint joint = JointFactory.CreateDistanceJoint(physMain.Simulator, player.BodyObject,
-                                                                   touchedObject.BodyObject, Vector2.Zero, Vector2.Zero);
-
-            joint.Frequency = 4.0f;
-            joint.DampingRatio = .5f;
-            joint.Breakpoint = breakpoint;
-            joint.CollideConnected = true;
-            joint.Broke += joint_Broke;
-            _joints.Add(joint);
-
-            //create tounge
-            
 
             //timer
             var timer = new DispatcherTimer {Interval = TimeSpan.FromMilliseconds(33)};
@@ -203,84 +216,191 @@ namespace slHackathonGame.Pages
                                   //reduce distance
                                   if (joint.Length <= 0f)
                                   {
-                                     
                                       timer.Stop();
                                   }
                                   joint.Length -= .1f;
                               };
             timer.Start();
         }
+
+        private void SetupDistanceJoint(object sender)
+        {
+            //remove old joints
+            if (_joints.Count > 0)
+            {
+                foreach (DistanceJoint jnt in _joints)
+                {
+                    _physicsController.DeleteObject(jnt);
+                }
+            }
+
+            const float breakpoint = 1000f;
+
+            //translate coordinates relative to screen
+            var touchedObject = sender as PhysicsSprite;
+            //var list = BoundaryHelper.GetPointsForElement(touchedObject.uiElement, e.ManipulationContainer, false);
+
+            //get reference to player
+            PhysicsSprite player = _physicsController.PhysicsObjects["player"];
+
+
+            //create distance joint between the two
+            DistanceJoint joint = JointFactory.CreateDistanceJoint(_physicsController.Simulator, player.BodyObject,
+                                                                   touchedObject.BodyObject, Vector2.Zero, Vector2.Zero);
+
+            joint.Frequency = 4.0f;
+            joint.DampingRatio = .5f;
+            joint.Breakpoint = breakpoint;
+            joint.CollideConnected = true;
+            joint.Broke += joint_Broke;
+            _joints.Add(joint);
+
+            //create tounge
+
+
+            //timer
+            var timer = new DispatcherTimer {Interval = TimeSpan.FromMilliseconds(33)};
+
+            timer.Tick += delegate
+                              {
+                                  //joint broke
+                                  if (!joint.Enabled)
+                                  {
+                                      timer.Stop();
+                                      _physicsController.DeleteObject(joint);
+                                      return;
+                                  }
+
+                                  //reduce distance
+                                  if (joint.Length <= 0f)
+                                  {
+
+                                      timer.Stop();
+                                      _physicsController.DeleteObject(joint);
+                                  }
+                                  joint.Length -= .1f;
+                              };
+            timer.Start();
+        }
+
         private void joint_Broke(Joint arg1, float arg2)
         {
             arg1.Enabled = false;
         }
 
-       
 
-
-        private void GestureListenerDragStarted(object sender, Microsoft.Phone.Controls.DragStartedGestureEventArgs e)
+        private void LayoutRootManipulationStarted(object sender, ManipulationStartedEventArgs e)
         {
-            //get drag start
-            _startDrag = e.GetPosition(LayoutRoot);
-             var physicsController =
-             LayoutRoot.GetValue(PhysicsControllerMain.PhysicsControllerProperty) as PhysicsControllerMain;
-
-            if(_finger == null)
-
+       
+            BehaviorCollection behaviorCollection = Interaction.GetBehaviors(_finger);
+            if (behaviorCollection.Count == 0)
             {
-                //create finger ellipse
-             var _physicsController = LayoutRoot.GetValue(PhysicsControllerMain.PhysicsControllerProperty) as PhysicsControllerMain;
-             _finger = new Ellipse { Name = "finger", Stroke = new SolidColorBrush(Colors.Red), Width = 50, Height = 50,StrokeThickness = 2};
-             var behaviorCollection = Interaction.GetBehaviors(_finger);
-             behaviorCollection.Add(new PhysicsObjectBehavior() { BoundaryElement = "finger", IsBullet = true });
-             _physicsController.AddPhysicsBody(_finger.GetValue(PhysicsObjectMain.PhysicsObjectProperty) as PhysicsObjectMain);
-              
-              
+                behaviorCollection.Add(new PhysicsObjectBehavior
+                                           {BoundaryElement = "finger",IsStatic =true,IsSensor = true});
+                _physicsController.AddPhysicsBody(
+                    _finger.GetValue(PhysicsObjectMain.PhysicsObjectProperty) as PhysicsObjectMain);
             }
+
+
+            PhysicsSprite finger = _physicsController.PhysicsObjects["finger"];
             
 
+            finger.Collision += FingerCollision;
+            finger.BodyObject.OnSeparation += BodyObject_OnSeparation;
 
-
-  
-           
-           
-        
-        }
-
-        private void GestureListenerDragCompleted(object sender, Microsoft.Phone.Controls.DragCompletedGestureEventArgs e)
-        {
-            if(_finger != null)
+            //finger phys object already created
+            if (finger != null)
             {
-                _finger.Stroke = new SolidColorBrush(Colors.Transparent);
+                finger.Position = new Vector2((float) (e.ManipulationOrigin.X),
+                                              (float) (e.ManipulationOrigin.Y));
             }
-            return;
-            var physicsController =
-             LayoutRoot.GetValue(PhysicsControllerMain.PhysicsControllerProperty) as PhysicsControllerMain;
-
-           /* MessageBox.Show(
-                String.Format(
-                    "Silverlight Touchpoint: {0}\n Farseer Vector:{1}\n Farseer ScreenTopLeft: {2}\nFarseer ScreenBottomRight:{3}\nFarseer WorldTopLeft{4}\nFarseer WorldBottomRight{5}", e.ManipulationOrigin.ToString(), physicsController.ScreenToWorld(e).ToString(), BoundaryHelperBox2d.ScreenTopLeft, BoundaryHelperBox2d.ScreenBottomRight, BoundaryHelperBox2d.WorldTopLeft, BoundaryHelperBox2d.WorldBottomRight));*/
 
 
-            var pt = new System.Windows.Point((int)e.GetPosition(LayoutRoot).X, (int)e.GetPosition(LayoutRoot).Y);
+          
+            
 
-            var letGoVector = physicsController.ScreenToWorld(pt);
-            MessageBox.Show(letGoVector.ToString());
+          
+
 
             e.Handled = true;
         }
 
-        private void GestureListenerDragDelta(object sender, Microsoft.Phone.Controls.DragDeltaGestureEventArgs e)
+        void BodyObject_OnSeparation(Fixture fixtureA, Fixture fixtureB)
         {
-
-            //move finger
-            var _physicsController = LayoutRoot.GetValue(PhysicsControllerMain.PhysicsControllerProperty) as PhysicsControllerMain;
-            var finger = _physicsController.PhysicsObjects["finger"];
-
-            finger.Position = new Vector2((int)e.GetPosition(LayoutRoot).X, (int)e.GetPosition(LayoutRoot).Y);
-
-
-
+           if(_player.BodyObject.FixtureList.Contains(fixtureA) || _player.BodyObject.FixtureList.Contains(fixtureB))
+           {
+               target.Stroke =  new SolidColorBrush(Colors.Transparent);
+               _touchingPlayer = false;
+           }
         }
+
+        private void FingerCollision(PhysicsSprite source, string collidedWith)
+        {
+            Debug.WriteLine(collidedWith);
+            if (collidedWith != "player") return;
+            _touchingPlayer = true;
+            //Debug.WriteLine("touchingPlayer");
+
+
+            target.Stroke = new SolidColorBrush(Colors.Red);
+            target.SetValue(Canvas.TopProperty, _player.Position.Y - (target.Height /2));
+            target.SetValue(Canvas.LeftProperty, _player.Position.X - (target.Width / 2));
+        }
+
+        private void LayoutRootManipulationCompleted(object sender, ManipulationCompletedEventArgs e)
+        {
+            
+            
+        }
+
+        private void LayoutRootManipulationDelta(object sender, ManipulationDeltaEventArgs e)
+        {
+            
+            
+            PhysicsSprite finger = _physicsController.PhysicsObjects["finger"];
+            
+
+            double x = finger.Position.X + e.DeltaManipulation.Translation.X;
+            double y = finger.Position.Y + e.DeltaManipulation.Translation.Y;
+
+            finger.Position = new Vector2((float) x,
+                                          (float) y);
+            _startDrag = finger.Position;
+
+
+            if (_dragLine == null)
+            {
+                _dragLine = new Line
+                {
+                    Stroke = new SolidColorBrush(Color.FromArgb(255, 254, 192, 200)),
+                    StrokeThickness = 4,
+                };
+            }
+
+            //set new line properties
+            _dragLine.StrokeThickness = 2;
+            _dragLine.X1 = _startDrag.X;
+            _dragLine.X2 = _startDrag.X - e.DeltaManipulation.Translation.X;
+            _dragLine.Y1 = _startDrag.Y;
+            _dragLine.Y2 = _startDrag.Y - e.DeltaManipulation.Translation.Y;
+            _dragLine.Name = "line";
+
+            if (!LayoutRoot.Children.Contains(_dragLine))
+            {
+                LayoutRoot.Children.Add(_dragLine);
+            }
+
+            
+        
+          
+
+
+            e.Handled = true;
+        }
+
+
+       
+        
+
     }
 }
---
+++ slHackathonGame/UserControls/Branch.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Xna.Framework;
+using Spritehand.FarseerHelper;
+using Point = System.Windows.Point;
+
+namespace slHackathonGame.UserControls
+{
+    public partial class Branch : UserControl
+    {
+        public Point Location { get; set; }
+        public Branch()
+        {
+            InitializeComponent();
+            Loaded += new RoutedEventHandler(BranchLoaded);
+        }
+
+        void BranchLoaded(object sender, RoutedEventArgs e)
+        {
+            var physicsController =
+                Parent.GetValue(PhysicsControllerMain.PhysicsControllerProperty) as PhysicsControllerMain;
+            if(physicsController != null)
+            physicsController.Initialized += PhysicsControllerInitialized;
+        }
+
+        void PhysicsControllerInitialized(object source)
+        {
+            var physicsController = source as PhysicsControllerMain;
+            var physObj = physicsController.PhysicsObjects[this.Name];
+            physObj.Position = new Vector2((float)Location.X, (float)Location.Y);
+            physicsController.Initialized -= PhysicsControllerInitialized;
+        }
+    }
+}
