e2b859c5299566c286dcac22b28375d5202c770c
Charles King <bringking@gmail.com>
2012-08-10 18:22:26 -0700

    Started adding functions for joints

--- slHackathonGame/Pages/GamePage.xaml.cs
+++ slHackathonGame/Pages/GamePage.xaml.cs
@@ -30,16 +30,47 @@ public partial class GamePage
             //get reference to branch
             if (physicsController != null)
             {
+                //get reference to bounding box
                 var bbox = physicsController.PhysicsObjects["bbox"];
 
                 //hook up collision event
                 bbox.Collision += BranchCollision;
             }
+
+            //get location they clicked on branch
+            var branch = physicsController.PhysicsObjects["branch"];
+            branch.ManipulationCompleted += branch_ManipulationCompleted;
+
+
+            //setup a joint to test physics
+            
+           // SetupPulleyJoint(physicsController);
         }
 
         void BranchCollision(PhysicsSprite source, string collidedWith)
         {
             //MessageBox.Show("Hit branch");
+        }
+
+        void SetupPulleyJoint(PhysicsControllerMain physMain)
+        {
+
+            //get reference to player
+            var player = physMain.PhysicsObjects["player"];
+      
+
+
+        }
+
+        void branch_ManipulationCompleted(object sender, System.Windows.Input.ManipulationCompletedEventArgs e)
+        {
+            //translate coordinates relative to screen
+            var physObject = sender as PhysicsSprite;
+            MessageBox.Show(physObject.Position.ToString());
+
+            MessageBox.Show(e.ManipulationOrigin.ToString());
+            
+            
         }
     }
 }
